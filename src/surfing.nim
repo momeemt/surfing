116868cc27a42825028ced07556eb38d5f63c3af
Mutsuha Asada <momiimt@gmail.com>
2021-09-19 03:28:08 +0900

    👍 Add: force simply

--- src/surfing.nim
+++ src/surfing.nim
@@ -2,10 +2,12 @@ from private/transformation as trans import nil
 from private/utils import nil
 
 proc decode (message: string, time: bool = false, simple: bool = false): int =
+  var time = (not simple) and time
   utils.timeMeasure(time):
     utils.echoResult("decode", trans.decode(message), simple)
 
 proc encode (message: string, num: uint = 1, max: int = high(int), time: bool = false, simple: bool = false): int =
+  var time = (not simple) and time
   utils.timeMeasure(time):
     if max == high(int):
       utils.echoResult("encode", trans.encode(message, num), simple)
