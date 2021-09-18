4c0e468512ed9e29b7321c39827ed84b08c89e38
Mutsuha Asada <momiimt@gmail.com>
2021-09-19 03:03:49 +0900

    👍 Add: implement time measuring

--- src/private/utils.nim
+++ src/private/utils.nim
@@ -1,2 +1,12 @@
+import times
+
+template timeMeasure* (condition: bool, body: untyped): untyped =
+  var start_times: Time
+  if condition:
+    start_times = getTime()
+  body
+  if condition:
+    echo "time: " & $(getTime() - start_times)
+
 proc hr* =
   echo "---------------------------------"
--- src/surfing.nim
+++ src/surfing.nim
@@ -1,18 +1,20 @@
 from private/transformation as trans import nil
 from private/utils import nil
 
-proc decode (message: string): int =
-  utils.hr()
-  echo "decode: " & trans.decode(message)
+proc decode (message: string, time: bool = false): int =
+  utils.timeMeasure(time):
+    utils.hr()
+    echo "decode: " & trans.decode(message)
 
-proc encode (message: string, num: uint = 1): int =
-  utils.hr()
-  echo "encode: " & trans.encode(message, num)
+proc encode (message: string, num: uint = 1, time: bool = false): int =
+  utils.timeMeasure(time):
+    utils.hr()
+    echo "encode: " & trans.encode(message, num)
 
 when isMainModule:
   import cligen
 
   dispatchMulti(
-    [decode, short = { "message": 'm' }],
-    [encode, short = { "message": 'm', "num": 'n' }]
+    [decode, short = { "message": 'm', "time": 't' }],
+    [encode, short = { "message": 'm', "num": 'n', "time": 't' }]
   )
